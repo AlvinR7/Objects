5540c174e9b801078ec77450ba70642434d43c59
AlvinR7 <36727415+AlvinR7@users.noreply.github.com>
2019-01-09 21:21:01 -0800

    Update characterpositioning.js

--- characterpositioning.js
+++ characterpositioning.js
@@ -1,14 +1,14 @@
 function countLetters(input) {
-  var input = input.split(" ").join("").split(""); // end result removing spaces from input
-  var countList = {};  // object
+  var input = input.split(" ").join("").split(""); 
+  var countList = {}; 
 
   for (var i = 0; i < input.length; i++) {
-    letter = input[i]; // redefining input[i]
-    if (countList[letter]) {  // determines if "l" is in the object
-      countList[letter].push(i); // push index value to object countList
+    letter = input[i];
+    if (countList[letter]) {  
+      countList[letter].push(i); 
     }
     else {
-      countList[letter] = [i]; // adds the letter l and value to the object
+      countList[letter] = [i]; 
     }
   }
   return countList;
